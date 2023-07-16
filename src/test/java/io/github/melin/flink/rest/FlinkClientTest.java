ff29f0555cd7125f3b1c3f13bbc82657660d90be
melin <libinsong1204@gmail.com>
2023-07-16 23:11:20 +0800

    update yaml

--- src/test/java/io/github/melin/flink/rest/FlinkClientTest.java
+++ src/test/java/io/github/melin/flink/rest/FlinkClientTest.java
@@ -1,17 +1,28 @@
 package io.github.melin.flink.rest;
 
 import io.github.melin.flink.rest.client.api.ApiClient;
-import io.github.melin.flink.rest.client.api.FlinkApi;
-import io.github.melin.flink.rest.client.model.JobIdsWithStatusOverview;
+import io.github.melin.flink.rest.client.api.FlinkRestClientApi;
+import io.github.melin.flink.rest.client.model.*;
 
 public class FlinkClientTest {
     public static void main(String[] args) {
         ApiClient apiClient = new ApiClient();
-        FlinkApi flinkApi = new FlinkApi(apiClient);
+        FlinkRestClientApi flinkApi = new FlinkRestClientApi(apiClient);
 
-        flinkApi.getApiClient().setBasePath("http://ip:8088/proxy/application_1688224869950_0118/");
-        JobIdsWithStatusOverview jobs = flinkApi.getJobs();
+        flinkApi.getApiClient().setBasePath("http://x.x.x.x:42300/v1");
+        MultipleJobsDetails jobs = flinkApi.getJobsOverview();
 
         System.out.println(jobs);
+
+        JobStatusInfo jobStatusInfo = flinkApi.getJobStatusInfo("68f9fd7069ace1f36ea5e01cc0c58719");
+
+        System.out.println(jobStatusInfo);
+
+        //flinkApi.getApiClient().setDebugging(true);
+        CheckpointTriggerRequestBody checkpointTriggerRequestBody = new CheckpointTriggerRequestBody();
+        checkpointTriggerRequestBody.checkpointType(CheckpointType.FULL);
+        TriggerResponse triggerResponse = flinkApi.triggerCheckpoint("68f9fd7069ace1f36ea5e01cc0c58719",
+                checkpointTriggerRequestBody);
+        System.out.println(triggerResponse);
     }
 }
